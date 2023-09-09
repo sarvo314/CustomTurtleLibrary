3cfb650d0ab226fbb0521cdd4dd341bd2d052c0e
Sarvottam <sarvottamswaroop100@gmail.com>
2023-09-09 17:55:04 +0530

    Added drawing delay

--- myTurtle/MyTurtle.py
+++ myTurtle/MyTurtle.py
@@ -1,9 +1,12 @@
 import math
+import time
+from threading import Thread
 import tkinter as tk
 WIDTH = 1280
 HEIGHT = 720
 
-
+#delays of 0.001 seconds
+SPEED = 0.1
 class MyTurtle():
     def __init__(self):
         self.window = tk.Tk("MyTurtle")
@@ -21,8 +24,9 @@ class MyTurtle():
         #   initial background color = white
         self.BGColor(1, 1, 1)
         self.canvas.pack()
+        self.current_distance = 0
         #   initial background color = white
-        # self.BGColor(1, 1, 1)
+                # self.BGColor(1, 1, 1)
         #   initially is pen is up
         self.pen_down = False
 
@@ -36,29 +40,35 @@ class MyTurtle():
             self.Left(theta)
 
     def BGColor(self, r, g, b):
-        self.BG_COLOR = [r*255, g*255, b*255]
+        self.BG_COLOR = [int(r*255), int(g*255), int(b*255)]
         bg_color = f"#{self.BG_COLOR[0]:02X}{self.BG_COLOR[1]:02X}{self.BG_COLOR[2]:02X}"
         self.window.configure(bg=bg_color)
         self.canvas.configure(bg=bg_color)
 
-    def find_final(self, distance):
-        curr_x = self.x
-        curr_y = self.y
+    def find_final(self, curr_x, curr_y, distance):
         final_x = self.dir[0]*distance + curr_x
         final_y = self.dir[1]*distance + curr_y
         return (final_x, final_y)
 
     def Draw(self, distance):
+        self.current_distance = 0
+        self.__draw_step(distance)
+
+    def __draw_step(self, distance_to_draw):
         curr_x = self.x
         curr_y = self.y
-        final_x, final_y = self.find_final(distance)
-        self.canvas.create_line(curr_x, curr_y, final_x, final_y, fill=self.line_color, width=self.line_width) 
-
+        while(self.current_distance < distance_to_draw):
+            final_x, final_y = self.find_final(curr_x, curr_y, SPEED) 
+            self.canvas.create_line(curr_x, curr_y, final_x, final_y, fill=self.line_color, width=self.line_width) 
+            curr_x = final_x
+            curr_y = final_y
+            self.current_distance += SPEED
+            self.canvas.update()
     def Forward(self, distance):
         #   check if pen is down
         if(self.pen_down):
             self.Draw(distance)
-        self.x, self.y = self.find_final(distance)
+        self.x, self.y = self.find_final(self.x, self.y, distance)
     #   alpha is the angle by which we are rotating
 
     def Left(self, alpha):
@@ -76,15 +86,15 @@ class MyTurtle():
     def Backward(self, distance):
         if(self.pen_down):
             self.Draw(self, distance)
-        self.x, self.y = self.find_final(distance)
+        self.x, self.y = self.find_final(self.x, self.y, distance)
 
     def PenDown(self, isDown):
         self.pen_down = isDown
 
     def LineColor(self, r, g, b):
-        red = r * 255
-        blue = b * 255
-        green = g * 255
+        red = int(r * 255)
+        blue = int(b * 255)
+        green = int(g * 255)
         line_color = f"#{red:02X}{green:02X}{blue:02X}"
         self.line_color = line_color 
 
--- square.py
+++ square.py
@@ -7,9 +7,15 @@ cursor.LineColor(1, 1, 1)
 cursor.PenDown(False)
 cursor.Forward(50)
 cursor.Left(90)
+print("here1")
 cursor.PenDown(True)
+#now we go forwards
 cursor.Forward(50)
+print("here2")
+#rotate and
 cursor.Left(90)
+#go forward
+print("rotate and go forward")
 cursor.Forward(100)
 cursor.Left(90)
 cursor.Forward(100)
